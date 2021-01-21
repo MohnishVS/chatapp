54d4e81858a7d50c88d75c8e8f6b4e28bc7ea258
MohnishVS <monishcena619@gmail.com>
2021-01-21 21:04:07 +0530

    chat app

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\MessageSentEvent;
+use App\Models\Message;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class MessageController extends Controller
+{
+   //
+}
